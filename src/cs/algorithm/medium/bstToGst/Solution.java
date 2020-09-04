95ebdc82b5f6be8e4b93b243d4224a603e8dd489
David Wang <david.wang@sis.ai>
2020-09-05 02:07:39 +0800

    feat: bst to gst

--- src/cs/algorithm/medium/bstToGst/Solution.java
+++ src/cs/algorithm/medium/bstToGst/Solution.java
@@ -2,15 +2,45 @@ package cs.algorithm.medium.bstToGst;
 
 import cs.algorithm.base.binaryTree.TreeNode;
 
+import java.util.ArrayList;
+
+/**
+ * Given the root of a binary search tree with distinct values,
+ * modify it so that every node has a new value equal to the sum of the values of the original tree
+ * that are greater than or equal to node.val.
+ * <p>
+ * As a reminder, a binary search tree is a tree that satisfies these constraints:
+ * <p>
+ * The left subtree of a node contains only nodes with keys less than the node's key.
+ * The right subtree of a node contains only nodes with keys greater than the node's key.
+ * Both the left and right subtrees must also be binary search trees.
+ * <p>
+ * Constraints:
+ * <p>
+ * The number of nodes in the tree is between 1 and 100.
+ * Each node will have value between 0 and 100.
+ * The given tree is a binary search tree.
+ * Note: This question is the same as 538: https://leetcode.com/problems/convert-bst-to-greater-tree/
+ */
 public class Solution {
     public static TreeNode bstToGst(TreeNode root) {
-        
-        return root;
+        if (root == null) {
+            return null;
+        }
+        return travel(root, new int[]{0});
     }
 
-    private static TreeNode travel(TreeNode root, Integer[] list) {
+    private static TreeNode travel(TreeNode root, int[] temp) {
+        if (root.right != null) {
+            travel(root.right, temp);
+        }
 
+        root.val += temp[0];
+        temp[0] = root.val;
 
+        if (root.left != null) {
+            travel(root.left, temp);
+        }
         return root;
     }
 }
--- src/cs/algorithm/medium/bstToGst/Test.java
+++ src/cs/algorithm/medium/bstToGst/Test.java
@@ -7,6 +7,6 @@ public class Test {
     public static void main(String[] args) {
         Integer[] input = {4, 1, 6, 0, 2, 5, 7, null, null, null, 3, null, null, null, 8};
         TreeNode greatSumTree = Solution.bstToGst(new BinarySearchTree(input).value);
-        System.out.println("end");
+        System.out.println("break point");
     }
 }

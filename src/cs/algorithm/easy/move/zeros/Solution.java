bbc0d40a10e7116cae4466eb67701cfc221f3b94
Pao-Sheng Wang <david840422@gmail.com>
2021-01-22 15:09:25 +0800

    move zeros

--- src/cs/algorithm/easy/move/zeros/Solution.java
+++ src/cs/algorithm/easy/move/zeros/Solution.java
@@ -2,6 +2,15 @@ package cs.algorithm.easy.move.zeros;
 
 public class Solution {
     public static void moveZeroes(int[] nums) {
-
+        int z = 0, temp;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                z++;
+                continue;
+            }
+            temp = nums[i - z];
+            nums[i - z] = nums[i];
+            nums[i] = temp;
+        }
     }
 }
--- src/cs/algorithm/easy/move/zeros/Test.java
+++ src/cs/algorithm/easy/move/zeros/Test.java
@@ -5,8 +5,8 @@ import java.util.Arrays;
 public class Test {
     public static void main(String[] args) {
         int[] a = {9, 0, 4, 6, 0, 7, 0, 1, 0};
-//		int[] a = {0,3,0,8,1,0,0,2,0};
-//		int[] a = {0,1,0,3,12};
+//        int[] a = {0, 3, 0, 8, 1, 0, 0, 2, 0};
+//        int[] a = {0, 1, 0, 3, 12};
         Solution.moveZeroes(a);
         Arrays.stream(a).forEach(v -> System.out.print(v + " "));
     }
